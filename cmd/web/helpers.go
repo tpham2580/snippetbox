14e57aaffdaf251946710dc57303d3fcab47b20d
Timothy Pham <tpham2580@gmail.com>
2024-11-24 15:12:24 -0800

    added centralize error handling

---
+++ cmd/web/helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+// serverError writes an error message and also includes the stack trace as well
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Println(trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// clientError sends the specific status code and description
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// Throws a 404 Not Found Error
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
