59d8794fb050d82863fa4f2fa1a679740541d4b3
Timothy Pham <tpham2580@gmail.com>
2024-11-24 15:27:59 -0800

    updated stack trace with correct file and changes home html back

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,7 +9,7 @@ import (
 // serverError writes an error message and also includes the stack trace as well
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
