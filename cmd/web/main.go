bbb60f9a928c221f1be9672600d7c639284da829
Tim <tpham2580@gmail.com>
2024-11-29 20:42:52 -0800

    moved around db variables into .env file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,18 +7,25 @@ import (
 	"net/http"
 	"os"
 
+	_ "github.com/joho/godotenv/autoload"
+	"snippetbox.timpham.net/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
+	envValue := os.Getenv("DB_PASS") // Grabs DB .env file
+	defaultDNS := "web:" + envValue + "@/snippetbox?parseTime=true"
+
 	// Flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dns := flag.String("dns", "web:P@ssword1@/snippetbox?parseTime=true", "MySQL data source name") // MySQL DSN string
+	dns := flag.String("dns", defaultDNS, "MySQL data source name") // MySQL DSN string
 	flag.Parse()
 
 	// INFO and ERROR logging
@@ -36,6 +43,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// Initialize a new http.Server struct to use the errorLog logger instead of standard logger
